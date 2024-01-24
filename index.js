866427f9c3d4aaa12595f51ecd46db59c550e915
Ian Bachiller <ianbachise@gmail.com>
2024-01-23 22:27:33 -0500

    Complete looping code along

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+const peeps = ["Josef", "Ian", "Mari"];
+const burtday = "birthday";
+
+function writeCards(people, event) {
+  const emptyArray = [];
+  for (let a = 0; a < people.length; a++) {
+    const b = `Thank you, ${people[a]}, for the wonderful ${event} gift!`;
+    emptyArray.push(b);
+    // debugger;
+  }
+  console.log(emptyArray);
+  return emptyArray;
+}
+writeCards(peeps, burtday);
+
+
+function countDown(integer) {
+    for (let a = integer; a>=0; a--){
+      console.log(a);
+    }
+    // return(z);
+  }
+    countDown(integer);
+  
